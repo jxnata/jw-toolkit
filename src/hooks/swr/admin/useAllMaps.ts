2f38306de8b843002a91dde7aee43f05e349d2e5
Jonatã Oliveira <jonata.96ol@gmail.com>
2024-03-09 14:35:08 -0300

    fix: 🐞 get all maps

--- src/hooks/swr/admin/useAllMaps.ts
+++ src/hooks/swr/admin/useAllMaps.ts
@@ -6,7 +6,7 @@ import { IMap } from 'types/models/Map'
 const fetcher = (url: string) => api.get(url).then(res => res.data)
 
 const useAllMaps = () => {
-	const { data, error, mutate } = useSWR('/maps?all', fetcher)
+	const { data, error, mutate } = useSWR('/maps/all', fetcher)
 
 	const maps: IMap[] = get(data, 'maps', [])
 
