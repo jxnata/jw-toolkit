5032620962037fac9d8080b9037e65fd4f277b4f
Jonatã Oliveira <jonata.96ol@gmail.com>
2024-04-10 15:03:27 -0300

    fix: 🐞 app store review

--- src/components/LocationRequest/index.tsx
+++ src/components/LocationRequest/index.tsx
@@ -1,5 +1,6 @@
 import { useForegroundPermissions } from 'expo-location'
 import { Redirect } from 'expo-router'
+import { Linking } from 'react-native'
 
 import * as S from './styles'
 
@@ -12,13 +13,25 @@ const LocationRequest = () => {
 		}
 	}
 
+	const requestLocationPermission = async () => {
+		if (status.canAskAgain === false) {
+			Linking.openSettings()
+			return
+		}
+
+		requestPermission()
+	}
+
 	return (
 		<S.Container>
 			<S.Content>
 				<S.Title>Localização</S.Title>
-				<S.Paragraph>É preciso permitir a leitura da sua localização para usar o aplicativo.</S.Paragraph>
-				<S.Button onPress={requestPermission}>
-					<S.ButtonTitle>Ativar</S.ButtonTitle>
+				<S.Paragraph>
+					É preciso permitir a leitura da sua localização para usar o aplicativo. Essa informação não é salva
+					fora desse dispositivo.
+				</S.Paragraph>
+				<S.Button onPress={requestLocationPermission}>
+					<S.ButtonTitle>Continuar</S.ButtonTitle>
 				</S.Button>
 			</S.Content>
 		</S.Container>
