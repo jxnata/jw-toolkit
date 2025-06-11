1877c107a98f6da37d6d7bf675b2e6131e7477fa
Jonatã Oliveira <jxnata@proton.me>
2025-06-11 16:22:57 -0300

    feat ✨: nativewind configuration

---
+++ src/hooks/use-themed-colors.ts
@@ -0,0 +1,10 @@
+import { colors } from '@utils/color-theme'
+import { useColorScheme } from 'react-native'
+
+export function useThemedColors() {
+	const colorScheme = useColorScheme()
+
+	return {
+		colors: colors[colorScheme || 'light'],
+	}
+}
